0aa2f26b1a20bf8027815d40efb7b3cb21ed51e7
JACKCHAN000 <61930699+JACKCHAN000@users.noreply.github.com>
2021-03-29 21:53:39 +0800

    Add files via upload

---
+++ translator.lua
@@ -0,0 +1,19 @@
+local json = require("json")
+local http = require("socket.http")
+local url = require("socket.url")
+http.TIMEOUT = 0.5
+
+local input = "hello"
+local sl = "en"
+local tl = "fr"
+local function make_url(input, sl, tl)
+   return 'https://translate.googleapis.com/translate_a/single?client=gtx&sl='.. sl ..'&tl='.. tl ..'&dt=t&q='.. url.escape(input)
+end
+local reply = http.request(make_url(input,sl,tl))
+local data = json.decode(reply)
+
+for i, v in ipairs(data) do
+    output = v[1][1]
+end
+print(sl, input)
+print(tl, output)
